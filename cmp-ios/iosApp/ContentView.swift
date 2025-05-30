a9d9a26b45ce368f225a4292f4a3b234e60eca7e
Hekmatullah <hekmatullah.aminullah2@gmail.com>
2025-05-30 14:50:39 +0100

    added singed workflow

--- cmp-ios/iosApp/ContentView.swift
+++ cmp-ios/iosApp/ContentView.swift
@@ -14,9 +14,7 @@ struct ContentView: View {
     var body: some View {
         ComposeView()
                 .ignoresSafeArea(edges: .all)
-                .ignoresSafeArea(.keyboard)
-        // .ignoresSafeArea(.keyboard)
-         Compose has own keyboard handler
+                .ignoresSafeArea(.keyboard) // .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
     }
 }
 
